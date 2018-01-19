ab5ea9b3555682fd0b2ffc4cca27b67eb7733fe2
TomaszSowa <tsowa974@gmail.com>
2018-01-19 06:30:03 +0100

    Dynamic Array Implementation.

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,67 @@
 package com.codecool.dynamicArrayDojo;
 
-// put your code here!
 public class DynamicIntArray {
 
+    private int size = 0;
+    private int[] array;
+
+    DynamicIntArray() {}
+
+    DynamicIntArray(int size) {
+        this.size = size;
+        array = new int[size];
+    }
+
+    public void add(int newItem) {
+
+        int[] newArray = new int[size + 1];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        newArray[size] = newItem;
+        size += 1;
+        array = newArray;
+    }
+
+    public void remove(int index) {
+
+        int[] newArray = new int[size - 1];
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+        for (int i = index + 1; i < size; i++) {
+            newArray[i - 1] = array[i];
+        }
+        size -= 1;
+        array = newArray;
+    }
+
+    public void insert(int index, int newItem) {
+
+        if (index > size) {
+            add(newItem);
+        } else {
+            int[] newArray = new int[size + 1];
+            for (int i = 0; i < index; i++) {
+                System.out.print(array[i]);
+                newArray[i] = array[i];
+            }
+            newArray[index] = newItem;
+            for (int i = index; i < size; i++) {
+                newArray[i + 1] = array[i];
+            }
+            size += 1;
+            array = newArray;
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        String result = "";
+        for (int i = 0; i < size; i++) {
+            result += " " + array[i];
+        }
+        return result;
+    }
 }
--- src/main/java/com/codecool/dynamicArrayDojo/Program.java
+++ src/main/java/com/codecool/dynamicArrayDojo/Program.java
@@ -3,6 +3,7 @@ package com.codecool.dynamicArrayDojo;
 public class Program {
 
     public static void main(String[] args) {
+
         System.out.println("Do not run me, run the TESTS!");
     }
 }
